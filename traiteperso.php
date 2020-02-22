e7041c1c2153552e6c76d1de401cae9af6c0f34a
Alan Akra <alanakra@hotmail.fr>
2020-02-22 19:14:22 +0100

    caractères spéciaux en entités HTML

--- traiteperso.php
+++ traiteperso.php
@@ -4,11 +4,14 @@
     var_dump($_GET);
     $manager = new PersonnageManager($db);
 
-    $perso = new Personnage(['name' => $_GET['name'], 'pv' => $_GET['pv'], 'atk' => $_GET['atk']]);
+    $name = htmlspecialchars($_GET['name']);
+    $pv = htmlspecialchars($_GET['pv']);
+    $atk = htmlspecialchars($_GET['atk']);
+
+    $perso = new Personnage(['name' => $name, 'pv' => $pv, 'atk' => $atk]);
         
     $manager -> createPersonnage($perso);
 
     header("Location: admin.php");
 
-    
 ?> 
